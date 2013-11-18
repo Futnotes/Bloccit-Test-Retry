db814136b5a2a814f72d2013942fa62af344526a
Ben Jennings <benjennings2003@yahoo.com>
2013-11-19 00:35:41 +0800

    Testing stuff in module

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,10 +2,10 @@
 CarrierWave.configure do |config| 
   config.fog_credentials = { 
     :provider               => 'AWS', 
-    :aws_access_key_id      => ENV['AKIAJTTASNWF665M24QQ'], 
-    :aws_secret_access_key  => ENV['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'] 
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], 
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] 
   } 
-  config.fog_directory  = ENV['bloccit-test-retry']
+  config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_public     = false 
 end 
 
--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(:version => 20131117115625) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "role"
-    t.string   "avavatar"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
