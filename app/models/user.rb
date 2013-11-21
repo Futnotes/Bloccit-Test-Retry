a770c81e60193a629eb564d898d50b3f862410d9
vagrant <vagrant@rails-dev-box>
2013-11-21 18:37:01 +0000

    Still struggling with the comments migation errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar
   has_many :posts
+  has_many :comments
   # attr_accessible :title, :body
 
   before_create :set_member
