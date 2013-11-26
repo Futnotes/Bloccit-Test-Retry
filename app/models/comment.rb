384b34687826d5e97819a653fbc3d48288d9dea3
Ben Jennings <benjennings2003@yahoo.com>
2013-11-26 10:26:46 +0800

    Interlude changes for registration, email permissions and styling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,7 +9,8 @@ after_create :send_favorite_emails
 
   def send_favorite_emails
     self.post.favorites.each do |favorite|
-      FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+      if favorite.user_id != self.user_id && favorite.user.email_favorites?
+        FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+      end
     end
   end
-end
