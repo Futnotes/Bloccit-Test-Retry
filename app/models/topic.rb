a687592421d74599d10200ad37fbe41df847e815
Ben Jennings <benjennings2003@yahoo.com>
2013-11-26 08:56:29 +0800

    Scoped topics for privacy

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,7 @@ class Topic < ActiveRecord::Base
   attr_accessible :description, :name, :public, :post
   has_many :posts, dependent: :destroy
 
-  scope: visible_to, lambda { |user| user ? scoped : where(public: true) }
+  scope :visible_to, lambda { |user| user ? scoped : where(public: true) }
+
+
 end
