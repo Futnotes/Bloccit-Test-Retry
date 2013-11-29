8ab7a02439771d701d351bf317123d34371f1a2a
Ben Jennings <benjennings2003@yahoo.com>
2013-11-29 15:27:20 +0800

    Created show / hide jQuery function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,15 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
-
-alert('Hello World');
+$(document).ready(function() {
+  $(".js-show-hide").click(function() {
+    var selector = "." + $(this).attr('data-selector');
+    if ($(selector).is(":visible")) {
+      $(selector).slideUp();
+    }
+    else {
+      $(selector).slideDown();
+    }
+    return false;
+  });
+});
