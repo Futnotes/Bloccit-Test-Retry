2bdf441fc2b99ab1316d0e52af8116b6d2a3dfa3
Ben Jennings <benjennings2003@yahoo.com>
2013-11-29 19:38:54 +0800

    Ajaxified comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,18 +8,22 @@ class CommentsController < ApplicationController
 
     @comment = current_user.comments.build(params[:comment])
     @comment.post = @post
+    @new_comment = Comment.new
 
     authorize! :create, @comment, message: "You need be signed in to do that."
+    
     if @comment.save
       flash[:notice] = "Comment was created."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      render 'posts/show'
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
     end
   end
 
-def destroy
+  def destroy
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
 
@@ -37,5 +41,5 @@ def destroy
     
     end  
   end
-  
+
 end
